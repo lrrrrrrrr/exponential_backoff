5be6d5ca4db6addd28bf242c4defb3e45b894349
Nikolai Sleta <nikolai.sleta@yahoo.com>
2021-11-11 02:36:31 +0100

    Code initial commit

---
+++ src/Backoff.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Backoff;
+
+class Backoff
+{
+    protected const INTERVAL = 2000;
+    protected const MAX_ATTEMPTS = 3;
+
+    protected $interval;
+    protected $maxAttempts;
+
+    public function __construct()
+    {
+        $this->maxAttempts = static::MAX_ATTEMPTS;
+        $this->interval = static::INTERVAL;
+    }
+
+    public function call(\Closure $retryableFunction, array $exceptionTypes)
+    {
+        if ($this->maxAttempts <= 0) {
+            throw new \RuntimeException('Max attempts should be more than 0');
+        }
+
+        $attempt = 0;
+
+        while ($this->maxAttempts > $attempt++) {
+            try {
+                return call_user_func($retryableFunction);
+            } catch (\Throwable $exception) {
+                if (
+                    $this->maxAttempts <= $attempt
+                || !$this->checkIfObjectTypeInArray($exception, $exceptionTypes)
+                ) {
+                    throw $exception;
+                }
+
+                usleep(pow(2, $attempt) * $this->interval * 1000);
+            }
+        }
+
+        return null;
+    }
+
+    public function setMaxAttempts($maxAttempts)
+    {
+        $this->maxAttempts = $maxAttempts;
+    }
+
+    public function setInterval($interval)
+    {
+        $this->interval = $interval;
+    }
+
+    protected function checkIfObjectTypeInArray($object, array $types)
+    {
+        return (bool)array_filter($types, function ($type) use ($object) {
+            return $object instanceof $type;
+        });
+    }
+}
---
+++ tests/BackoffTest.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace Backoff;
+
+
+use PHPUnit\Framework\TestCase;
+use \RuntimeException;
+
+class BackoffTest extends TestCase
+{
+    protected $calls;
+
+    protected function setUp(): void
+    {
+        $this->calls = 1;
+    }
+
+    public function testCallWithOneAttempt() {
+        $backoff = new Backoff();
+        $backoff->setInterval(2);
+        $backoff->setMaxAttempts(1);
+        $this->expectException(\Exception::class);
+        $backoff->call($this->retryableFunction(), [\Throwable::class]);
+    }
+
+
+    public function testCallWithNotSuitableExceptionType() {
+        $backoff = new Backoff();
+        $backoff->setInterval(2);
+        $this->expectException(\Exception::class);
+        $backoff->call($this->retryableFunction(), [RuntimeException::class]);
+    }
+
+
+    public function testRetry() {
+        $backoff = new Backoff();
+        $backoff->setInterval(2);
+
+        $this->assertEquals('done', $backoff->call($this->retryableFunction(), [\Throwable::class]));
+    }
+
+
+//    public function testInterval() {
+//        $backoff = new Backoff();
+//        $backoff->setMaxAttempts(3);
+//        $backoff->setInterval(2000);
+//
+//        $start = time();
+//        $backoff->call($this->retryableFunction(), [\Throwable::class]);
+//        $end = time();
+//        $this->assertEquals(12, $end-$start);
+//        $this->assertEquals('done', $backoff->call($this->retryableFunction(), [\Throwable::class]));
+//    }
+
+
+    protected function retryableFunction() {
+        return function () {
+            if ($this->calls < 3) {
+                $this->calls++;
+                throw new \Exception('Try again');
+            }
+
+            return 'done';
+        };
+    }
+
+}
